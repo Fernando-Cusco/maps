ecf27aaa2a3ebd839524f408307f0fbce487fafc
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-10-10 19:14:01 -0500

    [add] Marker Events
    Agregamos eventos al marcador y al mapa

--- app/src/main/java/ups/sistemas/MapsActivity.java
+++ app/src/main/java/ups/sistemas/MapsActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,6 +15,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -35,9 +37,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        //zoom minimo y maximo permitido
+        mMap.setMinZoomPreference(15);
+        mMap.setMaxZoomPreference(20);
+
         // Add a marker in Sydney and move the camera
         LatLng cuenca = new LatLng(-2.897482, -79.004537);
-        mMap.addMarker(new MarkerOptions().position(cuenca).title("Marcador en Cuenca-EC"));
+        mMap.addMarker(new MarkerOptions().position(cuenca).title("Marcador en Cuenca-EC").draggable(true));
 
         //posicion de la camara
         CameraPosition position = new CameraPosition.Builder()
@@ -48,5 +54,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .build();
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(position));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(cuenca));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Toast.makeText(MapsActivity.this, "Corto", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //cuando presionamos por un rato
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                Toast.makeText(MapsActivity.this, "Largo", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //cuando necesitos arrastrar el marcador
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+                Toast.makeText(MapsActivity.this, "empezando a arrastrar: "+marker.getPosition().latitude +" "+marker.getPosition().longitude, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+                Toast.makeText(MapsActivity.this, "arrastrando: "+marker.getPosition().latitude +" "+marker.getPosition().longitude, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                Toast.makeText(MapsActivity.this, "empezando a soltar: "+marker.getPosition().latitude +" "+marker.getPosition().longitude, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
