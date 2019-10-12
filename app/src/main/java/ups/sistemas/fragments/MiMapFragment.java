d8f23c70cc080793e09dd96e5476b5933383a8ca
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-10-11 20:23:17 -0500

    [add] get update position
    Obtenemos la posición actual en una actualización cuando nos movemos en intervalos de 1 segundo.

--- app/src/main/java/ups/sistemas/fragments/MiMapFragment.java
+++ app/src/main/java/ups/sistemas/fragments/MiMapFragment.java
@@ -3,9 +3,14 @@ package ups.sistemas.fragments;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -29,6 +34,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.security.Provider;
+
 import ups.sistemas.R;
 
 public class MiMapFragment extends Fragment implements OnMapReadyCallback, View.OnClickListener {
@@ -40,6 +47,9 @@ public class MiMapFragment extends Fragment implements OnMapReadyCallback, View.
 
     private FloatingActionButton fab;
 
+    private LocationManager locationManager;
+    private Location cuurentLocation;
+
 
     public MiMapFragment() {
         // Required empty public constructor
@@ -50,7 +60,7 @@ public class MiMapFragment extends Fragment implements OnMapReadyCallback, View.
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        rootView =  inflater.inflate(R.layout.fragment_mi_map, container, false);
+        rootView = inflater.inflate(R.layout.fragment_mi_map, container, false);
 
         fab = (FloatingActionButton) rootView.findViewById(R.id.idFab2);
         fab.setOnClickListener(this);
@@ -62,7 +72,7 @@ public class MiMapFragment extends Fragment implements OnMapReadyCallback, View.
         super.onViewCreated(view, savedInstanceState);
 
         mapView = (MapView) rootView.findViewById(R.id.mi_map);
-        if(mapView != null){
+        if (mapView != null) {
             mapView.onCreate(null);
             mapView.onResume();
             mapView.getMapAsync(this);
@@ -72,7 +82,8 @@ public class MiMapFragment extends Fragment implements OnMapReadyCallback, View.
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setMyLocationEnabled(true);
+        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        /*
         mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
             @Override
             public boolean onMyLocationButtonClick() {
@@ -80,6 +91,42 @@ public class MiMapFragment extends Fragment implements OnMapReadyCallback, View.
                 return false;
             }
         });
+         */
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);                                     //desactivamos el boton para el zoom de mi ubicacion
+
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Toast.makeText(getContext(), "Latitud: " + location.getLatitude() + " Longuitud: " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        });
+
     }
 
 
